24b8409cb73ffb5bec9180050fc01279bf6d4022
Riley <ring8255@gmail.com>
2022-04-08 18:35:32 +0800

    Initial commit

---
+++ Bill-Splitting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bill-Splitting
+//
+//  Created by 雷翎 on 2022/4/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
