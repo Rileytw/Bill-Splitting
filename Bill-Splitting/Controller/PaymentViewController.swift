14690e286d91bfb52cd01b056eb654ab77dbea0f
Riley <ring8255@gmail.com>
2022-04-19 17:34:37 +0800

    Add paymentViewController for setting payment methods

---
+++ Bill-Splitting/Controller/PaymentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PaymentViewController.swift
+//  Bill-Splitting
+//
+//  Created by 雷翎 on 2022/4/19.
+//
+
+import UIKit
+
+class PaymentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
