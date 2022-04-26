11eced88eee8f27782a47e66725dc58f9739fd4d
Riley <ring8255@gmail.com>
2022-04-26 09:15:01 +0800

    Establish basic UI of native login

--- Bill-Splitting/Controller/SignInViewController.swift
+++ Bill-Splitting/Controller/SignInViewController.swift
@@ -13,12 +13,23 @@ class SignInViewController: UIViewController {
     let authorizationButton = ASAuthorizationAppleIDButton(authorizationButtonType: .signIn, authorizationButtonStyle: .black)
     var user: UserData = UserData(appleId: nil, userId: "", userName: "", userEmail: "", group: nil, payment: nil)
     
+    var accountLabel = UILabel()
+    var passwordLabel = UILabel()
+    var accountTextField = UITextField()
+    var passwordTextField = UITextField()
+    var logInButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setSignInButton()
+        setAccount()
+        setAccountTextField()
+        setPassword()
+        setPasswordTextField()
+        setLoginButton()
+        setAppleSignInButton()
     }
     
-    func setSignInButton() {
+    func setAppleSignInButton() {
         view.addSubview(authorizationButton)
         authorizationButton.translatesAutoresizingMaskIntoConstraints = false
         setAuthorizationButtonConstraint()
@@ -37,9 +48,87 @@ class SignInViewController: UIViewController {
         authorizationController.performRequests()
     }
     
+    func setAccount() {
+        view.addSubview(accountLabel)
+        accountLabel.translatesAutoresizingMaskIntoConstraints = false
+        setAccountLabelConstraint()
+        accountLabel.text = "帳號"
+    }
+    
+    func setAccountTextField() {
+        view.addSubview(accountTextField)
+        accountTextField.translatesAutoresizingMaskIntoConstraints = false
+        setAccountTextFieldConstraint()
+        accountTextField.borderStyle = .roundedRect
+        accountTextField.layer.borderColor = UIColor.systemGray.cgColor
+        accountTextField.layer.borderWidth = 1
+    }
+    
+    func setPassword() {
+        view.addSubview(passwordLabel)
+        passwordLabel.translatesAutoresizingMaskIntoConstraints = false
+        setPassordLabelConstraint()
+        passwordLabel.text = "密碼"
+    }
+    
+    func setPasswordTextField() {
+        view.addSubview(passwordTextField)
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        setPasswordTextFieldConstraint()
+        passwordTextField.borderStyle = .roundedRect
+        passwordTextField.layer.borderColor = UIColor.systemGray.cgColor
+        passwordTextField.layer.borderWidth = 1
+    }
+    
+    func setLoginButton() {
+        view.addSubview(logInButton)
+        logInButton.translatesAutoresizingMaskIntoConstraints = false
+        setLoginButtonConstraint()
+        logInButton.setTitle("登入", for: .normal)
+        logInButton.backgroundColor = .white
+        logInButton.setTitleColor(.systemGray, for: .normal)
+        logInButton.layer.cornerRadius = 8.0
+        logInButton.layer.borderWidth = 1
+    }
+    
+    func setPassordLabelConstraint() {
+        passwordLabel.topAnchor.constraint(equalTo: accountLabel.bottomAnchor, constant: 20).isActive = true
+        passwordLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        passwordLabel.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        passwordLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func setAccountLabelConstraint() {
+        accountLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        accountLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40).isActive = true
+        accountLabel.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        accountLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func setAccountTextFieldConstraint() {
+        accountTextField.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        accountTextField.leadingAnchor.constraint(equalTo: accountLabel.trailingAnchor, constant: 20).isActive = true
+        accountTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        accountTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func setPasswordTextFieldConstraint() {
+        passwordTextField.topAnchor.constraint(equalTo: accountTextField.bottomAnchor, constant: 20).isActive = true
+        passwordTextField.leadingAnchor.constraint(equalTo: passwordLabel.trailingAnchor, constant: 20).isActive = true
+        passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40).isActive = true
+        passwordTextField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    func setLoginButtonConstraint() {
+        logInButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20).isActive = true
+        logInButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
+        logInButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        logInButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
     func setAuthorizationButtonConstraint() {
         authorizationButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0).isActive = true
-        authorizationButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 0).isActive = true
+        authorizationButton.topAnchor.constraint(equalTo: logInButton.bottomAnchor, constant: 20).isActive = true
         authorizationButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
         authorizationButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
