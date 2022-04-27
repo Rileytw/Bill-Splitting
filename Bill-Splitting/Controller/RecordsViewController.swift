9494d2a50237798bcfaa1130635c5b5e490c5a04
Riley <ring8255@gmail.com>
2022-04-27 12:15:22 +0800

    Get collection data of paidInfo and involvedInfo database for all groups

--- Bill-Splitting/Controller/RecordsViewController.swift
+++ Bill-Splitting/Controller/RecordsViewController.swift
@@ -11,20 +11,103 @@ import Lottie
 class RecordsViewController: UIViewController {
     
     private var animationView = AnimationView()
+    var groups: [GroupData] = []
+    var itemData: [ItemData] = []
+    var paidItem: [ExpenseInfo] = []
+    var involvedItem: [ExpenseInfo] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+//        getGroupData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        setAnimation()
+//        setAnimation()
+        getGroupData()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        animationView.stop()
+//        animationView.stop()
+    }
+    // MARK: - Get groups data first
+    func getGroupData() {
+        GroupManager.shared.fetchGroups(userId: userId, status: 0) { [weak self] result in
+            switch result {
+            case .success(let groups):
+                self?.groups = groups
+                // MARK: - Use group data to get item data from each group
+                self?.getBetweenGroupsItems()
+            case .failure(let error):
+                print("Error decoding userData: \(error)")
+            }
+        }
+    }
+
+// MARK: - Get items from all groups
+    func getBetweenGroupsItems() {
+        for groupData in self.groups {
+            ItemManager.shared.fetchGroupItemData(groupId: groupData.groupId) { [weak self] result in
+                switch result {
+                case .success(let items):
+                    self?.itemData += items
+                    
+                case .failure(let error):
+                    print("Error decoding userData: \(error)")
+                    
+                }
+                // MARK: - Get subCollection of item collection
+                self?.getItemExpense()
+            }
+        }
+    }
+    
+    // MARK: - Use GCD group to get two collection data at same time
+    func getItemExpense() {
+        let group = DispatchGroup()
+        
+        let firstQueue = DispatchQueue(label: "firstQueue", qos: .default, attributes: .concurrent)
+        group.enter()
+        firstQueue.async(group: group) {
+            self.paidItem.removeAll()
+            for item in self.itemData {
+                ItemManager.shared.fetchPaidItemsExpense(itemId: item.itemId) { [weak self] result in
+                    switch result {
+                    case .success(let items):
+                        self?.paidItem += items
+                    case .failure(let error):
+                        print("Error decoding userData: \(error)")
+                    }
+                }
+            }
+            group.leave()
+        }
+        
+        let secondQueue = DispatchQueue(label: "secondQueue", qos: .default, attributes: .concurrent)
+        group.enter()
+        secondQueue.async(group: group) {
+            self.involvedItem.removeAll()
+            for index in 0..<self.itemData.count {
+                ItemManager.shared.fetchInvolvedItemsExpense(itemId: self.itemData[index].itemId) { [weak self] result in
+                    switch result {
+                    case .success(let items):
+                        self?.involvedItem += items
+                    case .failure(let error):
+                        print("Error decoding userData: \(error)")
+                    }
+                }
+            }
+            group.leave()
+        }
+        
+        group.notify(queue: DispatchQueue.main) {
+            print("====paid:\(self.paidItem)")
+            print("====paidCount:\(self.paidItem.count)")
+            print("====count:\(self.itemData.count)")
+            print("====involved:\(self.involvedItem)")
+            print("====involvedCount:\(self.involvedItem.count)")
+        }
     }
     
     func setAnimation() {
@@ -37,4 +120,5 @@ class RecordsViewController: UIViewController {
         view.addSubview(animationView)
         animationView.play()
     }
+    
 }
