d6cd87e4f7fad46c6a7baccffae3c1bf058a1cc6
Riley <ring8255@gmail.com>
2022-05-19 09:54:39 +0800

    Add enum for buttonName

--- Bill-Splitting/Model/ButtonModel.swift
+++ Bill-Splitting/Model/ButtonModel.swift
@@ -7,7 +7,28 @@
 
 import Foundation
 import UIKit
+import SwiftUI
 
 struct ButtonModel {
     let title: String
+}
+
+enum GroupButton {
+    case allGroups
+    case multipleUsers
+    case personal
+    case close
+    
+    var buttonName: String {
+        switch self {
+        case .allGroups:
+            return "所有群組"
+        case .multipleUsers:
+            return "多人支付"
+        case .personal:
+            return "個人預付"
+        case .close:
+            return "封存群組"
+        }
+    }
 }
