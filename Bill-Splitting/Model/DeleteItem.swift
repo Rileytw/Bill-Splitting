232f2b828a23c04579ff06de26de36fa99cb47a6
Riley <ring8255@gmail.com>
2022-05-26 20:41:44 +0800

    Integrate delete item function to new file

--- Bill-Splitting/Model/DeleteItem.swift
+++ Bill-Splitting/Model/DeleteItem.swift
@@ -5,4 +5,71 @@
 //  Created by 雷翎 on 2022/5/24.
 //
 
-import Foundation
+import UIKit
+
+class DeleteItem {
+    static let shared = DeleteItem()
+    
+    private(set) var isItemDeleteSucces: Bool = false
+    
+    func deleteItem(groupId: String,
+                    itemId: String,
+                    item: ItemData,
+                    completion: @escaping () -> Void) {
+        let paidUserId = item.paidInfo?[0].userId ?? ""
+        let paidPrice = item.paidInfo?[0].price ?? 0
+        
+        let group = DispatchGroup()
+        group.enter()
+        DispatchQueue.global().async {
+            GroupManager.shared.updateMemberExpense(userId: paidUserId ,
+                                                    newExpense: 0 - paidPrice,
+                                                    groupId: groupId) { [weak self] result in
+                switch result {
+                case .success:
+                    self?.isItemDeleteSucces = true
+                case .failure:
+                    self?.isItemDeleteSucces = false
+                }
+                group.leave()
+            }
+        }
+        
+        guard let involedInfo = item.involedInfo else { return }
+        for user in 0..<involedInfo.count {
+            group.enter()
+            DispatchQueue.global().async {
+                GroupManager.shared.updateMemberExpense(userId: involedInfo[user].userId,
+                                                        newExpense: involedInfo[user].price,
+                                                        groupId: groupId) { [weak self] result in
+                    switch result {
+                    case .success:
+                        self?.isItemDeleteSucces = true
+                        group.leave()
+                    case .failure:
+                        self?.isItemDeleteSucces = false
+                        group.leave()
+                    }
+                }
+            }
+        }
+        
+        group.enter()
+        DispatchQueue.global().async {
+            ItemManager.shared.deleteItem(itemId: itemId) { [weak self] result in
+                switch result {
+                case .success:
+                    self?.isItemDeleteSucces = true
+                case .failure:
+                    self?.isItemDeleteSucces = false
+                }
+                group.leave()
+            }
+        }
+        
+        group.notify(queue: DispatchQueue.main) {
+            completion()
+        }
+    }
+}
+    
