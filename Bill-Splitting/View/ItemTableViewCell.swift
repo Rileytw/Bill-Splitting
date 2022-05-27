e59566bdca16aabebac888d850952a5c370c7112
Riley <ring8255@gmail.com>
2022-05-27 22:16:15 +0800

    Change property type of name in createItemCell function

--- Bill-Splitting/View/ItemTableViewCell.swift
+++ Bill-Splitting/View/ItemTableViewCell.swift
@@ -60,63 +60,39 @@ class ItemTableViewCell: UITableViewCell {
     }
     
     func mapItemCell(
-        item: ItemData, time: String, paidPrice: Double,
+        itemName: String, time: String, paidPrice: Double,
         involvedPrice: Double, involvedType: InvolvedType) {
             var description: PaidDescription
             var price: Double
         switch involvedType {
         case .paid:
-            if item.itemName == "結帳" {
+            if itemName == "結帳" {
                 description = .settleUpInvolved
                 price = paidPrice
-//                createItemCell(
-//                    time: time,
-//                    name: item.itemName,
-//                    description: PaidDescription.settleUpInvolved,
-//                    price: paidPrice)
                 paidDescription.textColor = .styleRed
             } else {
                 description = .paid
                 price = paidPrice
-//                createItemCell(
-//                    time: time,
-//                    name: item.itemName,
-//                    description: PaidDescription.paid,
-//                    price: paidPrice)
                 paidDescription.textColor = .styleGreen
             }
         case .involved:
-            if item.itemName == "結帳" {
+            if itemName == "結帳" {
                 description = .settleUpPaid
                 price = involvedPrice
-//                createItemCell(
-//                    time: time,
-//                    name: item.itemName,
-//                    description: PaidDescription.settleUpPaid,
-//                    price: involvedPrice)
                 paidDescription.textColor = .styleGreen
             } else {
                 description = .involved
                 price = involvedPrice
-//                createItemCell(time: time,
-//                               name: item.itemName,
-//                               description: PaidDescription.involved,
-//                               price: involvedPrice)
                 paidDescription.textColor = .styleRed
             }
         case .notInvolved:
             description = .notInvolved
             price = 0
-//            createItemCell(time: time,
-//                           name: item.itemName,
-//                           description: PaidDescription.notInvolved,
-//                           price: 0)
             paidDescription.textColor = .greenWhite
         }
-            
             createItemCell(
                 time: time,
-                name: item.itemName,
+                name: itemName,
                 description: description,
                 price: price)
     }
