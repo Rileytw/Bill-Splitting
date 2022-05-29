cbe40c24c76d50940b9478a06292a05cee739d4a
Riley <ring8255@gmail.com>
2022-05-29 16:18:27 +0800

    Add extension of UIView to stickSubView

--- Bill-Splitting/Extension/UIView+Extension.swift
+++ Bill-Splitting/Extension/UIView+Extension.swift
@@ -5,4 +5,25 @@
 //  Created by 雷翎 on 2022/5/28.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    
+    func stickSubView(_ objectView: UIView) {
+
+        objectView.removeFromSuperview()
+
+        addSubview(objectView)
+
+        objectView.translatesAutoresizingMaskIntoConstraints = false
+
+        objectView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+
+        objectView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+
+        objectView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+
+        objectView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+}
